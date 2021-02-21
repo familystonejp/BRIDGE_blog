9326630dcce361f0c5034006ef83ee7613cb27c3
familystonejp <familystonejp@yahoo.co.jp>
2021-02-21 14:02:21 +0900

    2-21 end

--- pj_blog/settings.py
+++ pj_blog/settings.py
@@ -49,8 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'pj_blog.urls'
@@ -122,6 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
